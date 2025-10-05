a97fed0ebbe083a69ef4732a14057247e0718993
kiranThyagarajan <af2kiran2002@gmail.com>
2025-10-05 19:07:19 +0200

    finished dkm

--- lab_amoro-master/student_scripts/biglide_models.py
+++ lab_amoro-master/student_scripts/biglide_models.py
@@ -53,8 +53,10 @@ def compute_A_B(q11, q12, q21, q22):
 
 
 def dkm(q11, q12, q21, q22, q11D, q21D):
-    xD = 0
-    yD = 0
+    A, B = compute_A_B(q11, q12, q21, q22)
+    psiD = np.linalg.inv(A) @ B @ np.array([q11D, q21D])
+    xD = psiD[0]
+    yD = psiD[1]
     return xD, yD
 
 
